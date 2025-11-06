dd3fad6a331aa6c0e4a49f185c628b98968e7ad2
dausten21 <dausten@rollins.edu>
2025-11-06 11:51:21 -0500

    Genre

---
+++ Genre.java
@@ -0,0 +1,16 @@
+public class Genre {
+    private String name;
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
